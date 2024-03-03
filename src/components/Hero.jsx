c8e70908dd3bc2251d35d2a67077ed1612b611d2
javiergenepaul <javiergenepaul@gmail.com>
2024-03-04 00:10:28 +0800

    change hero message landing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,16 +17,16 @@ const Hero = () => (
 
             <div className="flex flex-row justify-between items-center w-full">
                 <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100ox] leading-[75px]">
-                    The SLAN <br className="sm:block hidden" />{' '}
-                    <span className="text-gradient">Generation</span>
+                    Modern <br className="sm:block hidden" />{' '}
+                    <span className="text-gradient">Website</span>
                 </h1>
                 <div className="ss:flex hidden md:mr-4 mr-0">
                     <GetStarted />
                 </div>
             </div>
-            <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100ox] leading-[75px] w-full">
+            {/* <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100ox] leading-[75px] w-full">
                 IGNAN AKO!!!!
-            </h1>
+            </h1> */}
             <p className={`${styles.paragraph} max-w-[470px]`}>
                 Our team of experts uses a methodology to identify the credit cards most likely to
                 fit your needs. We examine annual percentage rates, annual fees.
